bcaee0e3ca71b56a77a445222bfcf8f3e708e5fe
arunasmazeika <arunas@timble.com>
2012-11-30 16:58:43 +0100

    Added database row object.
    Since the idea is to provide some sort of easy to use API for customers to implement their own plugins/code for integrating LOGman with other components, some validation will be required.
    
    re #60

---
+++ code/administrator/components/com_activities/databases/rows/activity.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @version        $Id$
+ * @package        Nooku_Components
+ * @subpackage     Activities
+ * @copyright      Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+/**
+ * Activities Database Row Class
+ *
+ * @author         Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
+ * @package        Nooku_Components
+ * @subpackage     Activities
+ */
+class ComActivitiesDatabaseRowActivity extends KDatabaseRowDefault
+{
+    public function save()
+    {
+
+        if (!in_array($this->application, $applications = array('admin', 'site'))) {
+            $this->setStatus(KDatabase::STATUS_FAILED);
+            $this->setStatusMessage('Wrong application value. Allowed values are: ' . implode(', ', $applications));
+            return false;
+        }
+
+        if (!in_array($this->type, $types = array('com'))) {
+            $this->setStatus(KDatabase::STATUS_FAILED);
+            $this->setStatusMessage('Wrong type value. Allowed values are: ' . implode(', ', $types));
+            return false;
+        }
+
+        return parent::save();
+    }
+}
