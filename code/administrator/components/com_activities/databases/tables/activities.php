d0ddb3605697b13514013ef064bef6792d57fd96
arunasmazeika <arunas@timble.net>
2013-01-16 13:48:09 +0100

    Added sync method. Made table syncable.

--- code/administrator/components/com_activities/databases/tables/activities.php
+++ code/administrator/components/com_activities/databases/tables/activities.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * @version		$Id: activities.php 1485 2012-02-10 12:32:02Z johanjanssens $
- * @package     Nooku_Components
- * @subpackage  Activities
- * @copyright	Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://www.nooku.org
+ * @version        $Id: activities.php 1485 2012-02-10 12:32:02Z johanjanssens $
+ * @package        Nooku_Components
+ * @subpackage     Activities
+ * @copyright      Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
  */
 
 /**
  * Activities Database Table Class
  *
- * @author      Israel Canasa <http://nooku.assembla.com/profile/israelcanasa>
- * @package    	Nooku_Components
- * @subpackage 	Activities
+ * @author         Israel Canasa <http://nooku.assembla.com/profile/israelcanasa>
+ * @package        Nooku_Components
+ * @subpackage     Activities
  */
 
 class ComActivitiesDatabaseTableActivities extends KDatabaseTableDefault
@@ -21,9 +21,18 @@ class ComActivitiesDatabaseTableActivities extends KDatabaseTableDefault
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-            'behaviors' => array('creatable', 'identifiable')
+            'behaviors' => array('creatable', 'identifiable', 'syncable')
         ));
 
         parent::_initialize($config);
+    }
+
+    /**
+     * Syncs persisted status of activity rows.
+     */
+    public function sync(KDatabaseQuery $query)
+    {
+        $this->getDatabase()
+            ->update('activities_activities', array('persisted' => 0), (string) $query);
     }
 }
