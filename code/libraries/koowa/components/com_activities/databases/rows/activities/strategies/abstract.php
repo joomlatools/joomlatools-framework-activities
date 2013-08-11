0bc3bf3e6c00dc82d3eceac615cd87a25afa6284
Johan Janssens <johan@timble.net>
2013-08-11 03:31:54 +0200

    Updated file headers.

--- code/libraries/koowa/components/com_activities/databases/rows/activities/strategies/abstract.php
+++ code/libraries/koowa/components/com_activities/databases/rows/activities/strategies/abstract.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @package     LOGman
- * @copyright   Copyright (C) 2011 - 2013 Timble CVBA. (http://www.timble.net)
- * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        http://www.joomlatools.com
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa-activities for the canonical source repository
  */
 abstract class ComActivitiesDatabaseRowActivityStrategyAbstract extends KObject implements ComActivitiesDatabaseRowActivityStrategyInterface
 {
--- code/libraries/koowa/components/com_activities/databases/tables/activities.php
+++ code/libraries/koowa/components/com_activities/databases/tables/activities.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package        Nooku_Components
- * @subpackage     Activities
- * @copyright      Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
- * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link           http://www.nooku.org
+ * Koowa Framework - http://developer.joomlatools.com/koowa
+ *
+ * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/koowa-activities for the canonical source repository
  */
 
 /**
