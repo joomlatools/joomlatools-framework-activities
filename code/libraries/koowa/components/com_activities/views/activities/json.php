163fe9e28ad2deab1c9a0b085c62131b28faa23d
Arunas Mazeika <arunas@timble.net>
2013-08-26 14:46:28 +0200

    re #58 Updated view.
    We now make use of the strategy package to properly render activity streams.

--- code/libraries/koowa/components/com_activities/views/activities/json.php
+++ code/libraries/koowa/components/com_activities/views/activities/json.php
@@ -19,115 +19,29 @@
 
 class ComActivitiesViewActivitiesJson extends KViewJson
 {
-    /**
-     * Get the list data
-     *
-     * @return array The array with data to be encoded to json
-     */
-    protected function _getList()
+    protected function _getItem(KDatabaseRowInterface $row)
     {
-        if ($this->getLayout() == 'stream') {
-            $list = $this->_getStream();
-        } else {
-            $list = parent::_getList();
+        if ($this->getLayout() == 'stream')
+        {
+            $item = $row->getStreamData();
         }
-        return $list;
+        else
+        {
+            $item = parent::_getItem($row);
+        }
+
+        return $item;
     }
 
-    /**
-     * Get the stream data
-     *
-     * @return array The array with data to be encoded to json
-     */
-    protected function _getStream()
+    protected function _getList(KDatabaseRowsetInterface $rowset)
     {
-        //Get the model
-        $model = $this->getModel();
+        $result = array();
 
-        $url = clone KRequest::url();
-
-        //Get the model state
-        $state = $model->getState();
-
-        $vars = array();
-        foreach ($state->toArray(false) as $var) {
-            if (!$var->unique) {
-                $vars[] = $var->name;
-            }
+        foreach ($rowset as $row)
+        {
+            $result[] = $this->_getItem($row);
         }
 
-        $data = array(
-            'version' => '1.0',
-            'href'    => (string) $url->setQuery(array_merge($url->getQuery(true), $state->toArray())),
-            'url'     => array(
-                'type'     => 'application/json',
-                'template' => (string) $url->toString(KHttpUrl::BASE) . '?{&' . implode(',', $vars) . '}',
-            ),
-            'offset'  => (int) $state->offset,
-            'limit'   => (int) $state->limit,
-            'total'   => 0,
-            'items'   => array()
-        );
-
-        if ($list = $model->getList()) {
-            $items = array();
-            foreach ($list as $item) {
-                $items[] = $this->_toStream($item);
-            }
-
-            $data = array_merge($data, array(
-                'total' => $model->getTotal(),
-                'items' => $items
-            ));
-        }
-
-        return $data;
-    }
-
-    /**
-     * Casts a row object into an activity stream entry.
-     *
-     * @param KDatabaseRowDefault $item
-     *
-     * @return array
-     */
-    protected function _toStream(KDatabaseRowDefault $item)
-    {
-        $id = array(
-            'tag:' . KRequest::get('server.HTTP_HOST', 'string'),
-            'id:' . $item->id
-        );
-
-        $base = KRequest::url()
-            ->toString(KHttpUrl::BASE);
-
-        return array(
-            'id'        => implode(',', $id),
-            'published' => $this->getService('com://admin/activities.template.helper.date')->format(array(
-                'date'   => $item->created_on,
-                'format' => 'C'
-            )),
-            'verb'      => $item->action,
-            'object'    => array(
-                'url' => $this->getService('koowa:http.url', array('url' => $base))->setQuery(array(
-                    'option' => $item->type . '_' . $item->package,
-                    'view'   => $item->name,
-                    'id'     => $item->row,
-                ))->toString()
-            ),
-            'target'    => array(
-                'url' => $this->getService('koowa:http.url', array('url' => $base))->setQuery(array(
-                    'option' => $item->type . '_' . $item->package,
-                    'view'   => $item->name,
-                ))->toString()
-            ),
-            'actor'     => array(
-                'url' => $this->getService('koowa:http.url', array('url' => $base))->setQuery(array(
-                    'option' => 'com_users',
-                    'view'   => 'user',
-                    'id'     => $item->created_by
-                ))->toString()
-            )
-        );
+        return $result;
     }
 }
