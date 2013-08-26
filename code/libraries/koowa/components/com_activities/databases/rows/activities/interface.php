c0e8da785e25a4209ae96a035be3ca96f1cb5a13
Arunas Mazeika <arunas@timble.net>
2013-08-26 15:04:11 +0200

    re #94 Added activity row interface.

---
+++ code/libraries/koowa/components/com_activities/databases/rows/activities/interface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package        Nooku_Components
+ * @subpackage     Activities
+ * @copyright      Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+interface ComActivitiesDatabaseRowActivityInterface
+{
+    /**
+     * Casts an activity row to a string.
+     *
+     * This string correspond to the message of the activity that the row represents.
+     *
+     * @param bool $html Whether the HTML (true) or plain text (false) version is returned.
+     *
+     * @return string The activity message string.
+     */
+    public function toString($html = true);
+
+    /**
+     * Activity stream data getter.
+     *
+     * @return array Associative array containing formatted activity stream data for the activity row.
+     */
+    public function getStreamData();
+}
