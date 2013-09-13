ae3a79cc538887b74614cf09dfea087d2222c7f5
Arunas Mazeika <arunas@timble.net>
2013-09-13 15:15:35 +0200

    re #33 Cleanup.

--- code/libraries/koowa/components/com_activities/controllers/activity.php
+++ code/libraries/koowa/components/com_activities/controllers/activity.php
@@ -19,9 +19,6 @@ class ComActivitiesControllerActivity extends ComKoowaControllerDefault
     {
         parent::__construct($config);
 
-        // TODO To be removed as soon as the problem with language files loading on HMVC calls is solved
-        JFactory::getLanguage()->load('com_activities', JPATH_ADMINISTRATOR);
-
         $this->registerCallback('before.add', array($this, 'setIp'));
     }
 
