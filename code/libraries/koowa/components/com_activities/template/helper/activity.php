27ac09a607fc811a34d6317d5c26144ab91b4ecf
Arunas Mazeika <arunas@timble.net>
2013-12-18 15:45:20 +0100

    re #33 The helper now takes care of properly formatting the message content depending on the rendering format that's requested.

--- code/libraries/koowa/components/com_activities/template/helper/activity.php
+++ code/libraries/koowa/components/com_activities/template/helper/activity.php
@@ -2,9 +2,9 @@
 /**
  * Koowa Framework - http://developer.joomlatools.com/koowa
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://github.com/joomlatools/koowa-activities for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://github.com/joomlatools/koowa-activities for the canonical source repository
  */
 
 /**
@@ -15,13 +15,96 @@
  */
 class ComActivitiesTemplateHelperActivity extends KTemplateHelperAbstract implements KObjectMultiton
 {
+    /**
+     * Renders an activity message.
+     *
+     * @param array $config An optional configuration array.
+     *
+     * @return string The rendered activity.
+     * @throws InvalidArgumentException
+     */
     public function message($config = array())
     {
         $config = new KObjectConfig($config);
-        $config->append(array(
-            'html' => true
-        ));
 
-        return $config->row->toString($config->html);
+        $config->append(array('format' => 'html'));
+
+        $renderer = '_render' . ucfirst($config->format);
+
+        if (!method_exists($this, $renderer))
+        {
+            throw new InvalidArgumentException('Renderer not found');
+        }
+
+        $row = $config->row;
+
+        if (!$row instanceof ComActivitiesDatabaseRowActivity)
+        {
+            throw new InvalidArgumentException('Activity row not found');
+        }
+
+        return $this->$renderer($row->getStrategy()->getMessage());
+    }
+
+    /** Activity message Html renderer.
+     *
+     * @param ComActivityMessageInterface $message The activity message.
+     *
+     * @return string The Html message.
+     */
+    protected function _renderHtml(ComActivitiesMessageInterface $message)
+    {
+        foreach ($message->getParameters() as $parameter)
+        {
+            $output = '<span class="text">' . $parameter->getText() . '</span>';
+
+            if ($parameter->isLinkable())
+            {
+                $link_attributes = $parameter->getLinkAttributes();
+
+                $url = $parameter->getUrl();
+                $view = $this->getTemplate()->getView();
+
+                if ($parameter->isRoutable())
+                {
+                    $url = $view->getRoute($url);
+                } else {
+                    $url = $view->getUrl()->toString(KHttpUrl::AUTHORITY) . '/' . $url;
+                }
+
+                $output = '<a ' . (empty($link_attributes) ? '' : $this->buildAttributes($link_attributes)) . ' href="' . $url . '">' . $output . '</a>';
+            }
+
+            $attribs = $parameter->getAttributes();
+
+            if (count($attribs))
+            {
+                $output = '<span ' . $this->buildAttributes($attribs) . '>' . $output . '</span>';
+            }
+
+            $parameter->setContent($output);
+        }
+
+        $html = $message->toString();
+        $html .= $message->getScripts(); // Append scripts.
+
+        return $html;
+    }
+
+    /**
+     * Activity message text renderer.
+     *
+     * @param ComActivitiesMessageInterface $message The activity message.
+     *
+     * @return string The text message.
+     */
+    protected function _renderText(ComActivitiesMessageInterface $message)
+    {
+        foreach ($message->getParameters() as $parameter)
+        {
+            $parameter->setContent($parameter->getText());
+        }
+
+        return $message->toString();
     }
 }
