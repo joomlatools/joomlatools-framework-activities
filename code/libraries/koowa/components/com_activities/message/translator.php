dd023103c3af053ff481ee51f99208c6a01fcbab
Arunas Mazeika <arunas@timble.net>
2014-04-30 19:41:24 +0200

    re #33 Implement multiton.

--- code/libraries/koowa/components/com_activities/message/translator.php
+++ code/libraries/koowa/components/com_activities/message/translator.php
@@ -13,7 +13,7 @@
  * @author  Arunas Mazeika <https://github.com/amazeika>
  * @package Koowa\Component\Activities
  */
-class ComActivitiesMessageTranslator extends ComKoowaTranslatorAbstract implements ComActivitiesMessageTranslatorInterface
+class ComActivitiesMessageTranslator extends ComKoowaTranslatorAbstract implements ComActivitiesMessageTranslatorInterface, KObjectMultiton
 {
     protected function _initialize(KObjectConfig $config)
     {
