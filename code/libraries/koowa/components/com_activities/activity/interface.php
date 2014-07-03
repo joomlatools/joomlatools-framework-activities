4de46a74aef6fe21995442b416cf21887c3cb77b
Arunas Mazeika <arunas@timble.net>
2014-07-03 14:27:07 +0200

    re #42 Re-factored interface.
    The interface now fully implements activity stream items as defined by the specification.

--- code/libraries/koowa/components/com_activities/activity/interface.php
+++ code/libraries/koowa/components/com_activities/activity/interface.php
@@ -14,6 +14,10 @@
  * story of a person performing an action on or with an object -- "Geraldine posted a photo to her album" or "John
  * shared a video". In most cases these components will be explicit, but they may also be implied.
  *
+ * This interface provides an interface for creating objects following the JSON Activity Streams 1.0 specification.
+ * It also extends the specification by providing an activity format setter and getter for consumers to be able to
+ * render activities from JSON activity stream data.
+ *
  * @see http://activitystrea.ms/specs/json/1.0/
  *
  * @author  Arunas Mazeika <https://github.com/amazeika>
@@ -22,54 +26,220 @@
 interface ComActivitiesActivityInterface
 {
     /**
-     * Get the activity message string format
+     * Activity format setter.
      *
-     * The message string format is a compact representation of the activity which also provides information about the
-     * parameters it may contain.
+     * @param ComActivitiesActivityFormatInterface $format
      *
-     * @return string The activity message string format.
+     * @return ComActivitiesActivityInterface
      */
-    public function getMessageFormat();
+    public function setFormat(ComActivitiesActivityFormatInterface $format);
 
     /**
-     * Activity message parameters getter.
+     * Activity format getter.
      *
-     * @return array An array of ComActivitiesActivityParameterInterface objects.
+     * @return string The activity string format.
      */
-    public function getMessageParameters();
+    public function getFormat();
 
     /**
-     * Activity stream objects getter.
+     * Activity content getter.
+     *
+     * @param string $string The activity content.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setContent($content);
+
+    /**
+     * Activity content getter.
+     *
+     * @return string|null The activity content, null if the activity does not have a content.
+     */
+    public function getContent();
+
+    /**
+     * Activity icon setter.
+     *
+     * @param ComActivitiesActivityMedialinkInterface $icon The activity icon.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setIcon(ComActivitiesActivityMedialinkInterface $icon);
+
+    /**
+     * Activity icon getter.
+     *
+     * @return ComActivitiesActivityMedialinkInterface|null The activity icon, null if the activity does not have an icon.
+     */
+    public function getIcon();
+
+    /**
+     * Activity ID setter.
+     *
+     * @param string $id The activity ID.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setId($id);
+
+    /**
+     * Activity ID getter.
+     *
+     * @return string The activity ID.
+     */
+    public function getId();
+
+    /**
+     * Activity published date setter.
+     *
+     * @param KDate $date The published date.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setPublished(KDate $date);
+
+    /**
+     * Activity published date getter.
+     *
+     * @return KDate The published date.
+     */
+    public function getPublished();
+
+    /**
+     * Activity title setter.
+     *
+     * @param string $title The activity title.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setTitle($title);
+
+    /**
+     * Activity title getter.
+     *
+     * @return string|null The activity title, null is the activity does not have a title.
+     */
+    public function getTitle();
+
+    /**
+     * Activity actor object setter.
+     *
+     * @param ComActivitiesActivityObjectInterface $actor The actor object.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setObjectActor(ComActivitiesActivityObjectInterface $actor);
+
+    /**
+     * Activity actor object getter.
+     *
+     * @return ComActivitiesActivityObjectInterface The actor object.
+     */
+    public function getObjectActor();
+
+    /**
+     * Activity object object setter.
+     *
+     * @param ComActivitiesActivityObjectInterface $object The activity object object.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setObjectObject(ComActivitiesActivityObjectInterface $object);
+
+    /**
+     * Activity object object getter.
+     *
+     * @return ComActivitiesActivityObjectInterface|null The object object, null if the activity does not have an object.
+     */
+    public function getObjectObject();
+
+    /**
+     * Activity target object setter.
+     *
+     * @param ComActivitiesActivityObjectInterface $target The target object.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setObjectTarget(ComActivitiesActivityObjectInterface $target);
+
+    /**
+     * Activity target object getter.
+     *
+     * @return ComActivitiesActivityObjectInterface|null The target object, null if the activity does no have a target.
+     */
+    public function getObjectTarget();
+
+
+    /**
+     * Activity generator object setter.
+     *
+     * @param ComActivitiesActivityObjectInterface $generator The generator object.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setObjectGenerator(ComActivitiesActivityObjectInterface $generator);
+
+    /**
+     * Activity generator object getter.
+     *
+     * @return ComActivitiesActivityObjectInterface The generator object.
+     */
+    public function getObjectGenerator();
+
+    /**
+     * Activity generator object setter.
+     *
+     * @param ComActivitiesActivityObjectInterface $generator The generator object.
+     *
+     * @return ComActivitiesActivityInterface
+     */
+    public function setObjectProvider(ComActivitiesActivityObjectInterface $generator);
+
+    /**
+     * Activity provider object getter.
+     *
+     * @return ComActivitiesActivityObjectInterface The provider object.
+     */
+    public function getObjectProvider();
+
+    /**
+     * Activity objects getter.
      *
      * @return Array An array of ComActivitiesActivityObjectInterface objects.
      */
-    public function getStreamObjects();
+    public function getObjects();
 
     /**
-     * Looks for the activity actor.
+     * Activity verb setter.
      *
-     * @return boolean True if found, false otherwise.
+     * @param string $verb The activity verb.
+     *
+     * @return ComActivitiesActivityInterface
      */
-    public function findActor();
+    public function setVerb($verb);
 
     /**
-     * Looks for the activity object.
+     * Activity verb getter.
      *
-     * @return boolean True if found, false otherwise.
+     * @return string The activity verb.
      */
-    public function findObject();
+    public function getVerb();
 
     /**
-     * Looks for the activity target.
+     * Activity story setter.
      *
-     * @return bool|null True if found, false if not found, null if the activity has no target.
+     * The activity story is the plain-text natural-language title or headline for the activity.
+     *
+     * @param string $story The activity story.
+     *
+     * @return ComActivitiesActivityInterface
      */
-    public function findTarget();
+    public function setStory($story);
 
     /**
-     * Casts an activity to a string.
+     * Activity story getter.
      *
-     * @return string The string representation of an activity
+     * @return string The activity story.
      */
-    public function toString();
+    public function getStory();
 }
