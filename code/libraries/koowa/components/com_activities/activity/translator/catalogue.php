aa616408510e47c9ce95063a253366f6e5aa684b
Ercan Ozkaya <ercan@timble.net>
2014-08-01 13:49:41 +0300

    Update docblocks

--- code/libraries/koowa/components/com_activities/activity/translator/catalogue.php
+++ code/libraries/koowa/components/com_activities/activity/translator/catalogue.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Koowa Framework - http://developer.joomlatools.com/koowa
+ * Nooku Framework - http://nooku.org/framework
  *
  * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://github.com/joomlatools/koowa-activities for the canonical source repository
+ * @link		http://github.com/nooku/nooku-activities for the canonical source repository
  */
 
 /**
