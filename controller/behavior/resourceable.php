aca9c49b7344edff9ce30e496ecf92a4d4098208
Arunas Mazeika <arunas@timble.net>
2016-07-18 17:01:13 +0200

    #1 Re-factored and renamed behavior to resourceable.
    1. Removed configuratble controller.
    2. Removed behavior action.
    3. UUID gets set by the Identifiable.

---
+++ controller/behavior/resourceable.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * @package     LOGman
+ * @copyright   Copyright (C) 2011 - 2016 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+
+/**
+ * Resourceable Controller Behavior
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Joomlatools\Component\LOGman
+ */
+class ComActivitiesControllerBehaviorResourceable extends KControllerBehaviorAbstract
+{
+    /**
+     * A list of actions for cleaning up resources
+     *
+     * @var array
+     */
+    protected $_actions;
+
+    /**
+     * Resource controller
+     *
+     * @var KControllerInterface
+     */
+    protected $_controller;
+
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_actions    = KObjectConfig::unbox($config->actions);
+    }
+
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'actions'    => array('delete')
+        ));
+
+        parent::_initialize($config);
+    }
+
+    protected function _afterAdd(KControllerContextInterface $context)
+    {
+        $entity = $context->result;
+
+        if ($entity instanceof ComActivitiesActivityInterface && $entity->getActivityObject())
+        {
+            $resource = $this->_getResource($entity);
+
+            $controller = $this->_getController();
+
+            if (!in_array($entity->action, $this->_actions))
+            {
+                $data = $this->_getData($entity);
+
+                if (!$resource->isNew())
+                {
+                    // Update resource if title changed.
+                    if ($resource->title != $entity->title) {
+                        $controller->id($resource->id)->edit($data);
+                    }
+                }
+                else $controller->add($data);
+            }
+            else $controller->id($resource->id)->delete();
+        }
+    }
+
+    /**
+     * Resource getter.
+     *
+     * @param KModelEntityInterface $entity The entity to get the resource from
+     *
+     * @return KModelEntityInterface|null The resource
+     */
+    protected function _getResource($entity)
+    {
+        $model = $this->_getController()->getModel();
+
+        $model->reset()->getState()->setValues(array(
+            'package'     => $entity->package,
+            'name'        => $entity->name,
+            'resource_id' => $entity->row
+        ));
+
+        return $model->fetch();
+    }
+
+    /**
+     * Entity data getter
+     *
+     * @param KModelEntityInterface $entity The entity to get data from
+     *
+     * @return array The entity data
+     */
+    protected function _getData(KModelEntityInterface $entity)
+    {
+        return array(
+            'package'     => $entity->package,
+            'name'        => $entity->name,
+            'resource_id' => $entity->row,
+            'title'       => $entity->title,
+        );
+    }
+
+    /**
+     * Resource controller getter.
+     *
+     * @return KControllerInterface The controller
+     */
+    protected function _getController()
+    {
+        if (!$this->_controller instanceof KControllerInterface)
+        {
+            $parts         = $this->getIdentifier()->toArray();
+            $parts['path'] = array('controller');
+            $parts['name'] = 'resource';
+
+            $identifier = $this->getIdentifier($parts);
+
+            $this->_controller = $this->getObject($identifier);
+        }
+
+        return $this->_controller;
+    }
+}
