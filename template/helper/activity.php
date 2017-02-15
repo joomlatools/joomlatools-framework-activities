161ef46993f603d29589dc38587fe7ccfdf35c4a
Arunas Mazeika <arunas@timble.net>
2017-02-15 11:32:38 +0100

    Update copyright headers

--- template/helper/activity.php
+++ template/helper/activity.php
@@ -2,7 +2,7 @@
 /**
  * Nooku Framework - http://nooku.org/framework
  *
- * @copyright   Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright   Copyright (C) 2011 - 2017 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://github.com/joomlatools/joomlatools-framework-activities for the canonical source repository
  */
